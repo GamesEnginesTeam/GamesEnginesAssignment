9233433afc6bdeb4f8827ce879f465812cbadbc2
DanielsDubstep35 <98469510+DanielsDubstep35@users.noreply.github.com>
2023-12-20 00:33:27 +0000

    Level Completed ✅
    Completed a level.
    
    Co-Authored-By: Lazyice1172 <78085794+Lazyice1172@users.noreply.github.com>
    Co-Authored-By: Keith <98464156+Kylreia@users.noreply.github.com>

---
+++ Models/Lava.gd
@@ -0,0 +1,22 @@
+extends Area3D
+
+@export_file("*.tscn")
+var target_scene : String
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_body_entered(body):
+	var scene_base : XRToolsSceneBase = XRTools.find_xr_ancestor(self, "*", "XRToolsSceneBase")
+	if not scene_base:
+		return
+		
+	# Start loading the target scene
+	scene_base.load_scene(target_scene)
